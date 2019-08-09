580a01ae86df4ee79c3ab487ad858dd027a44efa
Yuri Katayama <yurikbcarvalho@gmail.com>
2019-08-09 00:38:05 -0300

    Start

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public int numCheckPoint, numLapPlayer, lapsLimit;
+	public bool playerLapStarted, playerWins;
+	public float playerTime, lastTimePlayer, bestTimePlayer;
+
+	[Space (20)]
+	public bool EnemyVars;
+	//	[Space (20)]
+
+	public int numCheckPointEnemy, numLapEnemy, lapsLimitEnemy;
+	public bool enemyLapStarted, enemyWins;
+	public float enemyTime, lastTimeEnemy, bestTimeEnemy;
+
+	void Start () {
+		Time.timeScale = 1;
+
+		numLapPlayer = 0;
+		playerTime = 0f;
+		playerWins = false;
+
+		numLapEnemy = 0;
+		enemyTime = 0f;
+		enemyWins = false;
+
+	}
+
+	void Update () {
+		PlayerLapsCounter ();
+		EnemyLapsCounter ();
+	}
+
+	void PlayerLapsCounter () {
+		if (playerLapStarted) {
+			playerTime += Time.deltaTime;
+		}
+	}
+	void EnemyLapsCounter () {
+		if (playerLapStarted) {
+			playerTime += Time.deltaTime;
+		}
+	}
+}
